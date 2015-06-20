e3719eb65fbe9f3e2deb05b123740bcbacef7b46
Ca Pham <vanca.vnn@gmail.com>
2015-06-20 23:26:23 +0700

    init plugin

---
+++ plugin.rb
@@ -0,0 +1,8 @@
+# name: plugin_discourse_slider
+# about: create and view status of zendesk ticket within each topic
+# authors: Ca Pham <vanca.vnn@gmail.com>
+register_asset "javascripts/admin/templates/site_settings/setting_text.hbs"
+register_asset "javascripts/discourse/templates/discovery.hbs"
+
+register_asset "stylesheets/slider_styles.css"
+register_asset "js/slider.js"
